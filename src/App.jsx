424149ca60ac2d254b10ff435d25a5b0f7ea2993
Krisna Firdaus <krisnafirdaus@gmail.com>
2024-01-31 10:30:23 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Homepage from './pages/Homepage/Homepage'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(1)
+
+  return (
+    <>
+			<Homepage />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import CardComponent from '../../components/card/index'
+import Banner from '../../components/banner/Banner'
+
+const Homepage = () => {
+	return (
+		<div>
+			<CardComponent />
+		 	<Banner name="Krisna" />
+		</div>
+	)
+} 
+
+export default Homepage
