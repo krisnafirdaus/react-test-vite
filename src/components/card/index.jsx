28b12859700ae0bbea72f33af28c837dc0a857df
Krisna Firdaus <krisnafirdaus@gmail.com>
2024-01-31 11:45:46 +0700

    add tailwindcss

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,7 +1,18 @@
 const Card = () => {
 	return (
-		<div>
-			<h1>Card</h1>
+		<div className="max-w-sm rounded overflow-hidden shadow-lg">
+			<img className="w-full" src="https://images.unsplash.com/photo-1682685796444-acc2f5c1b7b6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Sunset in the mountains" />
+			<div className="px-6 py-4">
+				<div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+				<p className="text-gray-700 text-base">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+				</p>
+			</div>
+			<div className="px-6 pt-4 pb-2">
+				<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+				<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+				<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+			</div>
 		</div>
 	)
 }
--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import Card from '../../components/card/index'
+import './Detail.css'
 
 const Detail = () => {
 	const [angka, setAngka] = useState(0)
 
+	const handleKurang = () => {
+		if(angka > 0){
+			setAngka(prev => prev - 1)
+		}
+	}
+
 	return (
 		<div>
-			<button onClick={() => setAngka(prev => prev + 1)}>Click!!</button>
+			<Card />
+			<button className="up pt-5" style={{ backgroundColor: 'red', color: 'white' }} onClick={() => setAngka(prev => prev + 1)}>Click!!</button>
+			<button onClick={handleKurang}>Kurang</button>
 			<h1>{angka}</h1>
 		</div>
 	)
